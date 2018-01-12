12fe2d7ab92b0a6995754aae864311effaae430a
Feng Junxin <e0167325@nus.edu.sg>
2018-01-12 14:09:30 +0800

    Add project files.

---
+++ SA45Team6ADSSIS/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SA45Team6ADSSIS
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ SA45Team6ADSSIS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SA45Team6ADSSIS.MvcApplication" Language="C#" %>
